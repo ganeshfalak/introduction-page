4842e09a341f2ceaaacb82a5f13b3769cf0ff029
ganeshfalak <ganeshfalak01@gmail.com>
2022-05-05 02:14:06 +0530

    Initial upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function loadImage(id, tragetId) {
+    var el = document.getElementById(id);
+    var tragetIEl = tragetId ? document.getElementById(tragetId) : el;
+    var imageToLoad;
+
+    if (el.dataset.image) {
+        imageToLoad = el.dataset.image;
+    } else if (typeof el.currentSrc === 'undefined') {
+        imageToLoad = el.src;
+    } else {
+        imageToLoad = el.currentSrc;
+    }
+
+    if (imageToLoad) {
+        var img = new Image();
+        img.src = imageToLoad;
+        img.onload = function() {
+            tragetIEl.classList.add('is-loaded');
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadImage('wallpaper');
+    loadImage('pictureImage', 'picture');
+});
